4f3a7036d5d976eff580c99899ae551cd4285abf
Your Name <sinth92@hotmail.com>
2021-10-13 16:07:23 +0900

    commit check

---
+++ Frontend/blanlist_v1/src/Blan.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Blan extends React.Component{
+    render(){
+        return(
+            <div className = "Blan">
+                <input type="checkbox" id="blan0" name="blan0" value="blan0"/>
+                <label for blan0>Blan Component</label>
+            </div>
+        );
+    }
+}
+
+export default Blan;
